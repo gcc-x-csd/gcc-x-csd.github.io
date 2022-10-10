ea29fff9d64acdaca5c40360ae0aa35510e95a63
Anuj Das <anonboy1434@gmail.com>
2022-10-10 15:29:44 +0530

    updated JS

--- PYQP/dept-assets/js/options-target.js
+++ PYQP/dept-assets/js/options-target.js
@@ -1,13 +1,12 @@
 
-
-var e = document.getElementById("departments");
-var value = e.value;
-var text = e.options[e.selectedIndex].text;
-
+function visitDept() {
+    var e = document.getElementById("departments");
+    var value = e.value;
+    var text = e.options[e.selectedIndex].text;
+  
 console.log(text)
 
 
-function visitDept(text) {
     if(value == 1 && text === 'Computer Science') {
         fetch('comp-sc.html')
         .then(response=> response.text())
@@ -16,28 +15,28 @@ function visitDept(text) {
         console.log('Department of Computer Science Selected')
     }
     if(value == 2 && text === 'Physics') {
-        fetch('comp-sc.html')
+        fetch('physics.html')
         .then(response=> response.text())
         .then(texts=> document.querySelector('html').innerHTML = texts);
 
         console.log("Department of Physics Selected");
     }
     if(value == 3 && text === 'Chemistry') {
-        fetch('comp-sc.html')
+        fetch('chemistry.html')
         .then(response=> response.text())
         .then(texts=> document.querySelector('html').innerHTML = texts);
 
         console.log("Department of Chemistry Selected");
     }
     if(value == 4 && text === 'Mathematics') {
-        fetch('comp-sc.html')
+        fetch('maths.html')
         .then(response=> response.text())
         .then(texts=> document.querySelector('html').innerHTML = texts);
 
         console.log("Department of Mathematics Selected");
     }
     if(value == 5 && text === 'Statistics') {
-        fetch('comp-sc.html')
+        fetch('statistics.html')
         .then(response=> response.text())
         .then(texts=> document.querySelector('html').innerHTML = texts);
         
